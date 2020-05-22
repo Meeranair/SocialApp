32dd77faf5cf78185dd10bf5d044b0ebb60dd51b
Meera Nair <mnair@catalyte.io>
2020-05-22 12:39:48 -0700

    Use seed data when first running the app

--- SocialApp.API/Models/User.cs
+++ SocialApp.API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace SocialApp.API.Models
 {
     public class User
@@ -6,5 +9,14 @@ namespace SocialApp.API.Models
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DoB { get; set; }
+        public DateTime AccountCreated { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
